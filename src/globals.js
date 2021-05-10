2b65a4378d269a625fc8213500400f1e50916b83
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-10 09:46:54 -0300

    Adicionando arquivo com cores padrões do projeto

---
+++ src/globals.js
@@ -0,0 +1,11 @@
+export const defaultColor = '#D63F37';
+
+export const black = '#181516';
+
+export const gray100 = '#b4b7c6';
+
+export const gray500 = '#86848c';
+
+export const background = '#e8e8ec';
+
+export const white = '#fff';
