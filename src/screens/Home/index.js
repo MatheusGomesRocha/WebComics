aacc6993ad2f27ec565a8e8a125a9db7e7014682
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-10 15:36:28 -0300

    TrendingComics Area finalizada na tela Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -8,18 +8,58 @@ import {
     HomeContainer,
 
     SearchArea,
-    SearchInput
+    SearchInput,
+
+    TrendingComicsArea,
+    TrendingComicsHeader,
+    TrendingComicsTitle,
+    TrendingComicsButton,
+    TrendingComicsItem,
+    TrendingComicsItemImg,
+    TrendingComicsItemName,
+    TrendingComicsItemAuthor
 } from './styles';
+import { ScrollView } from 'react-native';
+
+const trendingComicsArray = [
+    {id: 1, img: require('../../assets/img/attack-titan.jpg'), title: 'Attack On Titan', author: 'Lucy'},
+    {id: 2, img: require('../../assets/img/berserk.jpg'), title: 'Berserk', author: 'Key'},
+    {id: 3, img: require('../../assets/img/black-clover.jpg'), title: 'Black Clover', author: 'Masashi'},
+    {id: 4, img: require('../../assets/img/bleach.jpg'), title: 'Bleach', author: 'Parang'},
+    {id: 5, img: require('../../assets/img/blue-exorcist.jpg'), title: 'Blue Exorcist', author: 'Shrimp'},
+    {id: 6, img: require('../../assets/img/boruto.jpg'), title: 'Boruto', author: 'Masashi'},
+    {id: 7, img: require('../../assets/img/death-note.jpg'), title: 'Death Note', author: 'Parang'},
+];
 
 export default () => {
     return(
         <HomeContainer>
-            
             <SearchArea>
                 <SearchIcon fill="#000" width={22} height={18} />
                 <SearchInput placeholderTextColor="#666" placeholder="Search Comic..." />
                 <AdjustIcon fill="#000" width={22} height={18} />
             </SearchArea>
+
+            <TrendingComicsArea>
+                <TrendingComicsHeader>
+                    <TrendingComicsTitle>Trending Comics</TrendingComicsTitle>
+
+                    <TrendingComicsButton>
+                        <MoreIcon fill="#666" width={22} height={18} />
+                    </TrendingComicsButton>
+                </TrendingComicsHeader>
+                
+                <ScrollView contentContainerStyle={{paddingHorizontal: 10}} horizontal={true}>
+                    {trendingComicsArray.map((item, k) => (
+                        <TrendingComicsItem key={k}>
+                            <TrendingComicsItemImg source={item.img} />
+                            <TrendingComicsItemName numberOfLines={1}>{item.title}</TrendingComicsItemName>
+                            <TrendingComicsItemAuthor numberOfLines={1}>by {item.author}</TrendingComicsItemAuthor>
+                        </TrendingComicsItem>
+                    ))}
+                </ScrollView>
+            </TrendingComicsArea>
+
         </HomeContainer>
     )
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components/native';
-import { white, background, black } from '../../globals';
+import { white, background, black, gray500, gray100 } from '../../globals';
 
 export const HomeContainer = styled.SafeAreaView`
     flex: 1;
     background: ${white};
-    padding: 20px;
+    padding: 20px 0;
 `;
 
 export const SearchArea = styled.View`
@@ -14,10 +14,46 @@ export const SearchArea = styled.View`
     height: 56px;
     border-radius: 50px;
     padding: 0 15px;
+    margin: 0 20px;
 `;
 export const SearchInput = styled.TextInput`
     flex: 1;
     color: ${black};
     padding-left: 10px;
     font-size: 16px;
+`;
+
+
+export const TrendingComicsArea = styled.View`
+    margin-top: 30px;
+`;
+export const TrendingComicsHeader = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0 20px;
+    margin-bottom: 25px;
+`;
+export const TrendingComicsTitle = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+    color: ${gray500};
+`;
+export const TrendingComicsButton = styled.TouchableOpacity``;
+export const TrendingComicsItem = styled.TouchableOpacity`
+    margin: 0 10px;
+`;
+export const TrendingComicsItemImg = styled.Image`
+    width: 150px;
+    height: 200px;
+    border-radius: 20px;
+`;
+export const TrendingComicsItemName = styled.Text`
+    font-weight: bold;
+    margin-top: 5px;
+    font-size: 16px;
+    width: 150px;
+`;
+export const TrendingComicsItemAuthor = styled.Text`
+    color: ${gray500};
+    width: 150px;
 `;
