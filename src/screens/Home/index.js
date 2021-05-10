35db252f36e7405753bc1be802f680a57e18e669
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-10 15:09:47 -0300

    Fazendo Search Input Area na tela Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import SearchIcon from '../../assets/svg/search.svg';
+import AdjustIcon from '../../assets/svg/adjust.svg';
+import MoreIcon from '../../assets/svg/more.svg';
+
 import {
-    HomeContainer
+    HomeContainer,
+
+    SearchArea,
+    SearchInput
 } from './styles';
 
 export default () => {
     return(
-        <HomeContainer></HomeContainer>
+        <HomeContainer>
+            
+            <SearchArea>
+                <SearchIcon fill="#000" width={22} height={18} />
+                <SearchInput placeholderTextColor="#666" placeholder="Search Comic..." />
+                <AdjustIcon fill="#000" width={22} height={18} />
+            </SearchArea>
+        </HomeContainer>
     )
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,3 +1,23 @@
 import styled from 'styled-components/native';
+import { white, background, black } from '../../globals';
 
-export const HomeContainer = styled.SafeAreaView``;
+export const HomeContainer = styled.SafeAreaView`
+    flex: 1;
+    background: ${white};
+    padding: 20px;
+`;
+
+export const SearchArea = styled.View`
+    background: ${background};
+    flex-direction: row;
+    align-items: center;
+    height: 56px;
+    border-radius: 50px;
+    padding: 0 15px;
+`;
+export const SearchInput = styled.TextInput`
+    flex: 1;
+    color: ${black};
+    padding-left: 10px;
+    font-size: 16px;
+`;
