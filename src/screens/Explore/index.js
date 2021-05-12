4233d2f007829171717ba03ca05ad61d43e2857a
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-12 19:20:34 -0300

    Finalizando design da tela Explore

--- src/screens/Explore/index.js
+++ src/screens/Explore/index.js
@@ -2,13 +2,29 @@ import React, { useState } from 'react';
 import { ScrollView } from 'react-native';
 
 import Header from '../../components/Header';
+import ClockIcon from '../../assets/svg/clock.svg';
+import KoreaIcon from '../../assets/svg/south-korea.svg';
+import JapanIcon from '../../assets/svg/japan.svg';
+import ChinaIcon from '../../assets/svg/china.svg';
 
 import {
     ExploreContainer,
 
     FilterArea,
     FilterItem,
-    FilterText
+    FilterText,
+
+    ComicsArea,
+    ComicsItem,
+    ComicsBackgroundImg,
+    ComicsChapter,
+    ComicsChapterText,
+    ComicsCategory,
+    ComicsCategoryText,
+    ComicsItemInfo,
+    ComicsItemName,
+    ComicsItemRelease,
+    ComicsItemReleaseText
 } from './styles';
 
 
@@ -17,6 +33,17 @@ const filterArray = [
     {id: 2, title: 'Manhwa'},
     {id: 3, title: 'Manhua'},
     {id: 4, title: 'Manga'},
+];
+
+const comicsArray = [
+    {id: 1, img: require('../../assets/img/demon-slayer.jpg'), name: 'Demon Slayer', category: 'Manga', lastChapter: '610', release: '45 minutes ago'},
+    {id: 2, img: require('../../assets/img/dororo.jpg'), name: 'Dororo', category: 'Manga', lastChapter: '174', release: '1 hour ago'},
+    {id: 3, img: require('../../assets/img/fairy-tail.jpg'), name: 'Fairy Tail', category: 'Manhwa', lastChapter: '47', release: '2 hours ago'},
+    {id: 4, img: require('../../assets/img/fullmetal.jpg'), name: 'Fullmetal Achimist', category: 'Manhua', lastChapter: '119', release: '2 hours ago'},
+    {id: 5, img: require('../../assets/img/god-of-highschool.jpg'), name: 'God Of Highschool', category: 'Manhwa', lastChapter: '258', release: '4 hours ago'},
+    {id: 6, img: require('../../assets/img/haikyuu.jpg'), name: 'Haikyuu', category: 'Manga', lastChapter: '517', release: '8 hours ago'},
+    {id: 7, img: require('../../assets/img/jujutsu-kaisen.jpg'), name: 'Jujutsu Kaisen', category: 'Manhua', lastChapter: '344', release: '2 days ago'},
+    {id: 8, img: require('../../assets/img/mob-pshycho.jpg'), name: 'Mob Psycho 100', category: 'Manhwa', lastChapter: '229', release: '1 week ago'},
 ];
 
 export default () => {
@@ -37,6 +64,39 @@ export default () => {
                         ))}
                     </ScrollView>
                 </FilterArea>
+
+                <ComicsArea>
+                    {comicsArray.map((item, k) => (
+                        <ComicsItem key={k}>
+                            <ComicsBackgroundImg imageStyle={{ borderRadius: 25}} source={item.img}>
+                                <ComicsChapter>
+                                    <ComicsChapterText>Chapter {item.lastChapter}</ComicsChapterText>
+                                </ComicsChapter>
+
+                                <ComicsCategory>
+                                    {item.category === 'Manga' &&
+                                        <JapanIcon width={22} height={20} />
+                                    }
+                                    {item.category === 'Manhwa' &&
+                                        <KoreaIcon width={22} height={20} />
+                                    }
+                                    {item.category === 'Manhua' &&
+                                        <ChinaIcon width={22} height={20} />
+                                    }
+                                    <ComicsCategoryText>{item.category}</ComicsCategoryText>
+                                </ComicsCategory>
+                            </ComicsBackgroundImg>
+
+                            <ComicsItemInfo>
+                                <ComicsItemName numberOfLines={1}>{item.name}</ComicsItemName>
+                                <ComicsItemRelease>
+                                    <ClockIcon fill="#000" width={22} height={22} />
+                                    <ComicsItemReleaseText>{item.release}</ComicsItemReleaseText>
+                                </ComicsItemRelease>
+                            </ComicsItemInfo>
+                        </ComicsItem>
+                    ))}
+                </ComicsArea>
 
             </ScrollView>
         </ExploreContainer>
--- src/screens/Explore/styles.js
+++ src/screens/Explore/styles.js
@@ -22,3 +22,64 @@ export const FilterItem = styled.TouchableOpacity`
 export const FilterText = styled.Text`
     color: ${props=>props.color || gray500};
 `;
+
+
+export const ComicsArea = styled.View`
+    margin: 30px 20px;
+`;
+export const ComicsItem = styled.TouchableOpacity`
+    margin-bottom: 30px;
+`;
+export const ComicsBackgroundImg = styled.ImageBackground`
+    height: 200px;
+    border-radius: 20px;
+`;
+export const ComicsChapter = styled.View`
+    background: ${white};
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    height: 36px;
+    width: 120px;
+    border-radius: 30px;
+`;
+export const ComicsChapterText = styled.Text`
+    color: ${gray500};
+`;
+export const ComicsCategory = styled.View`
+    background: ${white};
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    bottom: 15px;
+    left: 15px;
+    height: 36px;
+    width: 120px;
+    border-radius: 30px;
+`;
+export const ComicsCategoryText = styled.Text`
+    color: ${gray500};
+    margin-left: 10px;
+`;
+export const ComicsItemInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 10px;
+`;
+export const ComicsItemName = styled.Text`
+    font-weight: bold;
+    font-size: 18px;
+    width: 55%;
+`;
+export const ComicsItemRelease = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+export const ComicsItemReleaseText = styled.Text`
+    margin-left: 10px;
+    color: ${gray500};
+`;
