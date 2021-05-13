dc9b1c83e7de8470e6701c223dce95405710134c
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-13 18:13:49 -0300

    Finalizando tela ComicRead + finalizando projeto

--- src/screens/ComicRead/index.js
+++ src/screens/ComicRead/index.js
@@ -1,13 +1,41 @@
 import React, { useState } from 'react';
 
+import AngleDownIcon from '../../assets/svg/angle-down.svg';
+import AngleRightIcon from '../../assets/svg/angle-right.svg';
 import {
-    ComicReadContainer
+    ComicReadContainer,
+
+    ComicReadPage,
+    ComicReadPageValue,
+
+    ComicReadChangePageButton,
+
+    ComicReadChapter,
+    ComicReadChapterValue
 } from './styles';
 
 export default () => {
+    const [page, setPage] = useState(22);
+
     return(
         <ComicReadContainer>
+            <ComicReadPage>
+                <ComicReadPageValue>Page {page}</ComicReadPageValue>
+                <AngleDownIcon fill="#000" height={22} width={18} />
+            </ComicReadPage>
 
+            <ComicReadChangePageButton background={page === 1 && '#cecece'} disabled={page === 1 && true} onPress={() => setPage(page - 1)}>
+                <AngleRightIcon fill="#fff" height={22} width={18} />
+            </ComicReadChangePageButton>
+
+            <ComicReadChangePageButton style={{right: 0}} onPress={() => setPage(page + 1)}>
+                <AngleRightIcon fill="#fff" height={22} width={18} />
+            </ComicReadChangePageButton>
+
+            <ComicReadChapter>
+                <ComicReadChapterValue>157</ComicReadChapterValue>
+                <AngleDownIcon fill="#fff" height={22} width={18} />
+            </ComicReadChapter>
         </ComicReadContainer>
     )
 }
--- src/screens/ComicRead/styles.js
+++ src/screens/ComicRead/styles.js
@@ -1,7 +1,49 @@
 import styled from 'styled-components/native';
-import { white } from '../../globals';
+import { black, gray100, gray50, white } from '../../globals';
 
 export const ComicReadContainer = styled.SafeAreaView`
     flex: 1;
     background: ${white};
+`;
+
+export const ComicReadPage = styled.View`
+    background: ${gray50};
+    flex-direction: row;
+    align-self: flex-end;
+    align-items: center;
+    justify-content: space-around;
+    position: absolute;
+    right: 20px;
+    height: 42px;
+    width: 120px;
+    border-radius: 30px;
+`;
+export const ComicReadPageValue = styled.Text``;
+
+export const ComicReadChangePageButton = styled.TouchableOpacity`
+    background: ${props=>props.background || black};
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 40%;
+    height: 80px;
+    width: 60px;
+`;
+
+export const ComicReadChapter = styled.View`
+    background: ${black};
+    flex-direction: row;
+    align-self: center;
+    align-items: center;
+    justify-content: space-around;
+    position: absolute;
+    bottom: 20px;
+    height: 54px;
+    width: 150px;
+    border-radius: 30px;
+    padding: 0 15px;
+`;
+export const ComicReadChapterValue = styled.Text`
+    color: ${white};
+    font-size: 16px;
 `;
