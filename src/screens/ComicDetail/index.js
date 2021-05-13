e1b6c7d2336f3d88a01a26319cfc91c1dd456828
Matheus Gomes <matheusgomes192@hotmail.com>
2021-05-13 17:18:15 -0300

    Finalizando tela ComicDetail

--- src/screens/ComicDetail/index.js
+++ src/screens/ComicDetail/index.js
@@ -1,9 +1,10 @@
-import React, { useLayoutEffect } from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react';
+import { ScrollView, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import ChinaIcon from '../../assets/svg/china.svg';
 import ShareIcon from '../../assets/svg/share.svg';
+import HeartIcon from '../../assets/svg/heart.svg';
 
 import {
     ComicDetailContainer,
@@ -11,9 +12,36 @@ import {
     ComicImg,
     ComicCategory,
     ComicCategoryText,
+
+    ComicInfoArea,
+    SelectButtonArea,
+    SelectButton,
+    SelectButtonText,
+
+    ComicInfoHeader,
+    ComicInfoColumn,
+    ComicInfoName,
+    ComicInfoType,
+    ComicInfoFavoriteButton,
+
+    ComicSecondaryInfo,
+    ComicSecondaryItem,
+    ComicSecondaryItemTitle,
+    ComicSecondaryItemText,
+
+    ComicDescription,
+
+    ComicAuthor, 
+    ComicAuthorImg,
+    ComicAuthorColumn,
+    ComicAuthorTitle,
+    ComicAuthorName,
+    ComicReadButton,
+    ComicReadButtonText,
 } from './styles';
 
 export default () => {
+    const [select, setSelect] = useState('informations')
     const navigation = useNavigation();
 
     useLayoutEffect(() => {
@@ -28,14 +56,71 @@ export default () => {
 
     return(
         <ComicDetailContainer>
-            
+            <ScrollView>
+                <ComicImg imageStyle={{borderRadius: 20}} source={require('../../assets/img/fairy-tail.jpg')}>
+                    <ComicCategory>
+                        <ChinaIcon width={22} height={18} />
+                        <ComicCategoryText>Manhwa</ComicCategoryText>
+                    </ComicCategory>
+                </ComicImg>
 
-            <ComicImg imageStyle={{borderRadius: 20}} source={require('../../assets/img/fairy-tail.jpg')}>
-                <ComicCategory>
-                    <ChinaIcon width={22} height={18} />
-                    <ComicCategoryText>Manhwa</ComicCategoryText>
-                </ComicCategory>
-            </ComicImg>
+                <ComicInfoArea>
+                    <SelectButtonArea>
+                        <SelectButton background={select === 'informations' && '#fff'} onPress={() => setSelect('informations')}>
+                            <SelectButtonText>Informations</SelectButtonText>
+                        </SelectButton>
+                        <SelectButton background={select === 'chapter' && '#fff'} onPress={() => setSelect('chapter')}>
+                            <SelectButtonText>Chapter</SelectButtonText>
+                        </SelectButton>
+                    </SelectButtonArea>
+
+                    <ComicInfoHeader>
+                        <ComicInfoColumn>
+                            <ComicInfoName>Fairy Tail</ComicInfoName>
+                            <ComicInfoType>Action, Comedy, Dragons, Fight</ComicInfoType>
+                        </ComicInfoColumn>
+
+                        <ComicInfoFavoriteButton>
+                            <HeartIcon fill="#fff" width={30} height={30} />
+                        </ComicInfoFavoriteButton>
+                    </ComicInfoHeader>
+
+                    <ComicSecondaryInfo>
+                        <ComicSecondaryItem>
+                            <ComicSecondaryItemTitle>Rating</ComicSecondaryItemTitle>
+                            <ComicSecondaryItemText>4.5</ComicSecondaryItemText>
+                        </ComicSecondaryItem>
+
+                        <ComicSecondaryItem>
+                            <ComicSecondaryItemTitle>Chapter</ComicSecondaryItemTitle>
+                            <ComicSecondaryItemText>150+</ComicSecondaryItemText>
+                        </ComicSecondaryItem>
+
+                        <ComicSecondaryItem>
+                            <ComicSecondaryItemTitle>Language</ComicSecondaryItemTitle>
+                            <ComicSecondaryItemText>ENG</ComicSecondaryItemText>
+                        </ComicSecondaryItem>
+                    </ComicSecondaryInfo>
+
+                    <ComicDescription>
+                        The Journey to the martial peak is a lonely, solitary and long one, in the face
+                        of adversity, you must survive and remain undefected. 
+                    </ComicDescription>
+
+                    <ComicAuthor>
+                        <ComicAuthorImg source={require('../../assets/img/author3.jpg')} />
+
+                        <ComicAuthorColumn>
+                            <ComicAuthorTitle>Author</ComicAuthorTitle>
+                            <ComicAuthorName>by Shrimp</ComicAuthorName>
+                        </ComicAuthorColumn>
+
+                        <ComicReadButton>
+                            <ComicReadButtonText>Read Now</ComicReadButtonText>
+                        </ComicReadButton>
+                    </ComicAuthor>
+                </ComicInfoArea>
+            </ScrollView>
         </ComicDetailContainer>
     )
 }
--- src/screens/ComicDetail/styles.js
+++ src/screens/ComicDetail/styles.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components/native';
-import { white } from '../../globals';
+import { black, defaultColor, gray100, gray50, gray500, white } from '../../globals';
 
 export const ComicDetailContainer = styled.SafeAreaView`
     flex: 1;
     background: ${white};
-    padding: 0 20px;
 `;
 
 export const ComicImg = styled.ImageBackground`
     margin-top: 20px;
     height: 200px;
+    margin: 0 20px;
 `;
 export const ComicCategory = styled.View`
     background: ${white};
@@ -25,4 +25,118 @@ export const ComicCategory = styled.View`
 `;
 export const ComicCategoryText = styled.Text`
     margin-left: 5px;
+`;
+
+
+export const ComicInfoArea = styled.View`
+    flex: 1;
+    border-color: #ddd;
+    border-top-left-radius: 50px;
+    border-top-right-radius: 50px;
+    border-top-width: 1px;
+    border-left-width: 1px;
+    border-right-width: 1px;
+    margin-top: 30px;
+    padding: 0 20px;
+`;
+export const SelectButtonArea = styled.View`
+    background: ${gray50};
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 5px 10px;
+    border-radius: 30px;
+    margin-top: 30px;
+`;
+export const SelectButton = styled.TouchableOpacity`
+    background: ${props=>props.background || 'transparent'};
+    align-items: center;
+    justify-content: center;
+    width: 48%;
+    height: 42px;
+    border-radius: 30px;
+`;
+export const SelectButtonText = styled.Text``;
+
+export const ComicInfoHeader = styled.View`
+    flex-direction: row;
+    margin-top: 40px;
+`;
+export const ComicInfoColumn = styled.View`
+`;
+export const ComicInfoName = styled.Text`
+    font-size: 24px;
+`;
+export const ComicInfoType = styled.Text`
+    color: ${gray500};
+    margin-top: 5px;
+`;
+export const ComicInfoFavoriteButton = styled.TouchableOpacity`
+    background: ${defaultColor};
+    align-items: center;
+    justify-content: center;
+    width: 62px;
+    height: 62px;
+    border-radius: 36px;
+    margin-left: auto;
+`;
+
+export const ComicSecondaryInfo = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 40px;
+`;
+export const ComicSecondaryItem = styled.View`
+    align-items: center;
+    justify-content: center;
+    width: 30%;
+    height: 70px;
+    border: 1px solid ${gray50};
+    border-radius: 10px;
+`;
+export const ComicSecondaryItemTitle = styled.Text`
+    font-size: 16px;
+`;
+export const ComicSecondaryItemText = styled.Text`
+    color: ${gray100};
+    margin-top: 5px;
+`;
+
+export const ComicDescription = styled.Text`
+    margin-top: 30px;
+    color: ${gray500};
+    font-size: 15px;
+    line-height: 22px;
+`;
+
+export const ComicAuthor = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-top: 25px;
+`;
+export const ComicAuthorImg = styled.Image`
+    height: 50px;
+    width: 50px;
+    border-radius: 25px;
+`;
+export const ComicAuthorColumn = styled.View`
+    justify-content: center;
+    margin-left: 10px;
+`;
+export const ComicAuthorTitle = styled.Text`
+    font-size: 13px;
+    color: ${gray500};
+`;
+export const ComicAuthorName = styled.Text``;
+export const ComicReadButton = styled.TouchableOpacity`
+    background: ${black};
+    align-items: center;
+    justify-content: center;
+    margin-left: auto;
+    height: 48px;
+    width: 150px;
+    border-radius: 25px;
+`;
+export const ComicReadButtonText = styled.Text`
+    color: ${white};
+    font-weight: bold;
 `;
